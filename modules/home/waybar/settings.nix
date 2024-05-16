08f061d17e5d2fa1ffe77a638b6bfb3733bc3fde
jpuf0 <jpuf@jpuf.xyz>
2024-05-16 14:30:17 +0100

    lets try this :sob:

--- modules/home/waybar/settings.nix
+++ modules/home/waybar/settings.nix
@@ -25,7 +25,7 @@
     clock = {
       format = " {:%H:%M (%Z)}";
       tooltip = "true";
-      tooltip-format = "<big>{:%a %d %B %Y}\n{:%H:%M (%Z)}</big>\n<tt><small>{calendar}</small></tt>";
+      tooltip-format = "<big>{:%a %d %B %Y %H:%M (%Z)}</big>\n<tt><small>{calendar}</small></tt>";
       format-alt = " {:%d/%m}";
       timezones = [
         "Europe/London"
