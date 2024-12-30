98a3f42e4503ca94ee8811e4f9229ea6e4c5026f
Jpuf0 <jpuf@jpuf.xyz>
2024-12-30 19:16:40 +0000

    missing packages duh

--- hosts/azalea/hardware-configuration.nix
+++ hosts/azalea/hardware-configuration.nix
@@ -11,7 +11,7 @@
   boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usb_storage" "usbhid" "sd_mod" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-amd" "wl" "v4l2loopback" ];
-  boot.extraModulePackages = [
+  boot.extraModulePackages = with config.boot.kernelPackages; [
     v4l2loopback
   ];
 
