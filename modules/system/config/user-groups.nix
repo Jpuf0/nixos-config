823c41849eeff71bed5da647ad7e97df3ad67350
Jpuf0 <jpuf@jpuf.xyz>
2024-12-30 19:23:52 +0000

    add ypuf to users

--- modules/system/config/user-groups.nix
+++ modules/system/config/user-groups.nix
@@ -14,4 +14,20 @@
       "plugdev"
     ];
   };
+
+  users.users.ypuf = {
+    isNormalUser = true;
+    description = "i cant have colon 3";
+    initialPassword = "nixos";
+    shell = pkgs.zsh;
+    extraGroups = [
+      "networkmanager"
+      "wheel"
+      "docker"
+      "input"
+      "video"
+      "adbusers"
+      "plugdev"
+    ];
+  };
 }
