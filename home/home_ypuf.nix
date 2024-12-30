408cdc6c580f3f1e35f5256f8c06615439d3d4c8
Jpuf0 <jpuf@jpuf.xyz>
2024-12-30 19:22:27 +0000

    mkDefault

--- home/home_ypuf.nix
+++ home/home_ypuf.nix
@@ -1,8 +1,8 @@
-{
+{lib, ...}: {
   programs.home-manager.enable = true;
 
   home = {
     username = "ypuf";
-    homeDirectory = "/home/ypuf";
+    homeDirectory = lib.mkDefault "/home/ypuf";
   };
 }
