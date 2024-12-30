6f103e8a79d9ca3ff8b5def47c2ec1e2e4924cfc
Jpuf0 <jpuf@jpuf.xyz>
2024-12-30 20:04:20 +0000

    switch to amd

--- home/modules/programs/hypr/hyprland/config.nix
+++ home/modules/programs/hypr/hyprland/config.nix
@@ -4,10 +4,10 @@
 # debug:disable_logs = false
 $mainMod = SUPER
 
-monitor=Unknown-1,disable
-monitor=DP-2,1920x1080@240,0x0,1
-monitor=HDMI-A-1,1920x1080@60,1920x0,1
-# monitor = , preferred, auto, 1
+#monitor=Unknown-1,disable
+#monitor=DP-2,1920x1080@240,0x0,1
+#monitor=HDMI-A-1,1920x1080@60,1920x0,1
+monitor = , preferred, auto, 1
 # monitor=eDP-1,preferred,auto,2
 
 # autostart
@@ -142,9 +142,9 @@ animations {
   animation = workspaces, 1, 4, easeOutCubic, fade # styles: slide, slidevert, fade, slidefade, slidefadevert
 }
 
-render {
-  explicit_sync = 0
-}
+#render {
+#  explicit_sync = 0
+#}
 
 
 # ----------------------------------------------------------------
